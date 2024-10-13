2896a88080758a8a13a0ae6cf416ada168d06000
Kamal-Tester <meghani58155@gmail.com>
2024-10-14 00:22:41 +0530

    Commit

--- src/main/java/Naukri_refresh/Naukri_refresh/Naukri.java
+++ src/main/java/Naukri_refresh/Naukri_refresh/Naukri.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+import org.testng.annotations.Test;
 public class Naukri {
 	public static WebDriver driver;
 @Test
